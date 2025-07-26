7617e108c5c8b9c97ca66213fd23135a96633a93
Kateryna Koliada <6parkseolhwa6@gmail.com>
2025-07-26 15:27:53 +0200

    Add api requests

--- src/home.js
+++ src/home.js
@@ -1,1 +1,11 @@
 //Логіка сторінки Home
+
+import {
+  getCategories,
+  getProducts,
+  getProductsByCategory,
+} from './js/products-api';
+
+console.log(getCategories());
+console.log(getProducts(2));
+console.log(getProductsByCategory('beauty'));
---
+++ src/js/constants.js
@@ -0,0 +1,10 @@
+export const API_ENDPOINTS = {
+  CATEGORIES: 'products/category-list',
+  PRODUCTS_BY_CATEGORY: 'products/category/',
+  PRODUCTS: 'products',
+  PRODUCT_BY_ID: 'products/',
+  SEARCH: 'products/search',
+};
+
+export const ITEMS_PER_PAGE = 12;
+export const API_BASE_URL = 'https://dummyjson.com/';
